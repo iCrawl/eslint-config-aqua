7baab84525bfbcd5fb28e07dec04d6aad9e722c0
Crawl <icrawltogo@gmail.com>
2020-01-19 18:01:18 +0100

    Update svelte.js

--- svelte.js
+++ svelte.js
@@ -3,12 +3,7 @@ const path = require('path');
 module.exports = {
 	extends: path.join(__dirname, 'index.js'),
 	parserOptions: {
-		ecmaVersion: 2019,
 		sourceType: 'module'
-	},
-	env: {
-		es6: true,
-		browser: true
 	},
 	plugins: [
 		'svelte3'
