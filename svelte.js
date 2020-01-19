4ff3bd1bc45ce3f74af73f865dbd7dcf77604786
iCrawl <icrawltogo@gmail.com>
2020-01-19 18:25:51 +0100

    chore: update svelte cnfig

--- svelte.js
+++ svelte.js
@@ -3,21 +3,16 @@ const path = require('path');
 module.exports = {
 	extends: path.join(__dirname, 'index.js'),
 	parserOptions: {
-		sourceType: 'module'
+		sourceType: 'module',
 	},
-	plugins: [
-		'svelte3'
-	],
+	plugins: ['svelte3'],
 	overrides: [
 		{
-			files: [
-				'**/*.svelte'
-			],
-			processor: 'svelte3/svelte3'
-		}
+			files: ['**/*.svelte'],
+			processor: 'svelte3/svelte3',
+		},
 	],
 	settings: {
-		'import/code-modules': ['svelte'],
 		'svelte3/ignore-styles': attributes => attributes.lang,
-	}
+	},
 };
