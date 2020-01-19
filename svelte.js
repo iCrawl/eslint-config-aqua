867666e52ec1505471879a305ac3dc4e89a7a35c
Pitu <heyitspitu@gmail.com>
2020-01-19 13:50:38 +0900

    add svelte support

---
+++ svelte.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	extends: path.join(__dirname, 'index.js'),
+	parserOptions: {
+		ecmaVersion: 2019,
+		sourceType: 'module'
+	},
+	env: {
+		es6: true,
+		browser: true
+	},
+	plugins: [
+		'svelte3'
+	],
+	overrides: [
+		{
+			files: [
+				'**/*.svelte'
+			],
+			processor: 'svelte3/svelte3'
+		}
+	],
+	settings: {
+		'import/code-modules': ['svelte']
+	}
+};
